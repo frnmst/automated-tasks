67415cdd7224ff21a2f39bb8180ee36cf6e6e31e
Franco Masotti <franco.masotti@live.com>
2019-07-11 19:37:35 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,58 @@
+# automated-tasks
+
+A collection of scripts I have written and/or adapted that I currently
+use on my systems as automated tasks.
+
+## Table of contents
+
+<!--TOC-->
+
+- [automated-tasks](#automated-tasks)
+  - [Table of contents](#table-of-contents)
+  - [What](#what)
+  - [Why](#why)
+  - [Structure](#structure)
+  - [See also](#see-also)
+  - [LICENSE](#license)
+
+<!--TOC-->
+
+## What
+
+Automated tasks run when certain conditions are met, such as a particular time and
+date or when, for example, a USB flash drive is added to the system.
+
+## Why
+
+This repository is a way to improve the quality of these scripts.
+
+## Structure
+
+Scripts are divided by topic and placed in different directories
+accordingly. Have a look at the `./metadata.yaml` file.
+
+## See also
+
+- [From crontabs to Systemd timers](https://frnmst.gitlab.io/notes/from-crontabs-to-systemd-timers.html)
+  to learn where and how to place the files.
+
+## LICENSE
+
+Unless noted differently:
+
+```
+Copyright (C) 2019 Franco Masotti <franco.masotti@live.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+```
---
+++ drives/smartd_test.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2019 Franco Masotti <franco.masotti@live.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import re
+import configparser
+import sys
+import os
+import pathlib
+import subprocess
+import json
+import shlex
+
+# Constants.
+# These need to be checked.
+STATUS_IDLE=0
+STATUS_BUSY=249
+
+# You must stop programs like hd-idle before performing these tests.
+
+class UserNotRoot(Exception):
+    """The user running the script is not root."""
+
+class NoDisksConfigured(Exception):
+    """No disks configured."""
+
+def get_disks():
+    disks=list()
+    for d in pathlib.Path('/dev/disk/by-id').iterdir():
+        # Ignore disks ending with part-${integer} to avoid duplicates (names
+        # corresponding to the same disk).
+        disk = str(d)
+        if re.match('.+-part[0-9]+$', disk) is None:
+            try:
+               ddict = json.loads(subprocess.run(shlex.split('smartctl --all --json ' + shlex.quote(disk)), capture_output=True, check=True, timeout=30).stdout)
+               try:
+                   # Check for smart test support.
+                   if ddict['ata_smart_data']['capabilities']['self_tests_supported']:
+                       disks.append(disk)
+               except KeyError:
+                   pass
+            except subprocess.TimeoutExpired:
+                print('timeout for ' + disk)
+
+    return disks
+
+def disk_ready(disk: str):
+    # Raises a KeyError if disk has not S.M.A.R.T. status capabilities.
+    ddict = json.loads(subprocess.run(shlex.split('smartctl --all --json ' + shlex.quote(disk)), capture_output=True, check=True, timeout=30).stdout)
+    if ddict['ata_smart_data']['self_test']['status']['value'] != STATUS_BUSY:
+        return True
+    else:
+        return False
+
+def run_test(disk: str, test_length: str = 'long'):
+    return subprocess.run(shlex.split('smartctl --test=' + shlex.quote(test_length) +  ' ' + shlex.quote(disk)), capture_output=True, check=True, timeout=30).stdout
+
+if __name__ == '__main__':
+    if os.getuid() != 0:
+        raise UserNotRoot
+
+    configuration_file = shlex.quote(sys.argv[1])
+    config = configparser.ConfigParser()
+    config.read(configuration_file)
+
+    # Do not prepend '/dev/disk/by-id/'.
+    disks_to_check = shlex.quote(sys.argv[2])
+    disks_available = get_disks()
+
+    if len(config.sections()) == 0:
+        raise NoDisksConfigured
+
+    for s in config.sections():
+        dev = '/dev/disk/by-id/' + s
+        if config.getboolean(s, 'enable') and dev in disks_available:
+            if disks_to_check == 'all' or disks_to_check == s:
+                if disk_ready(dev):
+                    print ('attempting ' + s + ' ...')
+                    print (run_test(dev, config[s]['test']).decode('utf-8'))
+                else:
+                    # Drop test requests if a disk is running a test in a particular moment.
+                    # This avoid putting the disks under too much stress.
+                    print ('disk ' + s + ' not ready, checking the next...')
