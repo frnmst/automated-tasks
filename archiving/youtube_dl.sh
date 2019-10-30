2e001d7bf7ffd50f421a3edb43567f4c476e9df8
Franco Masotti <franco.masotti@live.com>
2019-10-30 19:17:04 +0100

    Added a notification system for the youtube_dl.sh script.

--- archiving/youtube_dl.sh
+++ archiving/youtube_dl.sh
@@ -8,7 +8,14 @@ CONFIG="${1}"
 mkdir -p "${OUTPUT_DST_DIR}"
 
 pushd "${OUTPUT_DST_DIR}"
+
+# Count the number of initial and final files.
+number_of_initial_files=$(wc -l "${ARCHIVE_LIST}" | awk '{print $1}')
+
+set +e
+# youtube-dl might not return 0 even if some videos are correctly downloaded.
 youtube-dl --verbose --config-location "${YTDL_CONFIG}" --batch "${URLS}" --download-archive "${ARCHIVE_LIST}"
+set -e
 
 if [ "${DELETE_OLD_FILES}" = 'true' ]; then
     # For this to work be sure to set the no-mtime option in the options file.
@@ -19,4 +26,18 @@ if [ "${DELETE_OLD_FILES}" = 'true' ]; then
     find . -type f -mtime +$((${DAYS_TO_KEEP}+1)) -exec rm {} \;
 fi
 
+number_of_final_files=$(wc -l "${ARCHIVE_LIST}" | awk '{print $1}')
+downloaded_files=$((${number_of_final_files}-${number_of_initial_files}))
+message=""${MESSAGE_PREAMBLE}" "${downloaded_files}" "${MESSAGE_POSTAMBLE}""
+if [ "${LOG_TO_STDOUT}" = 'true' ]; then
+    printf "%s\n" "${message}"
+fi
+if [ "${LOG_TO_GOTIFY}" = 'true' ]; then
+    curl -X POST \
+        ""${GOTIFY_URL}"/message?token="${GOTIFY_TOKEN}"" \
+        -F "title=${GOTIFY_TITLE}" \
+        -F "message=${message}" -F "priority="${GOTIFY_PRIORITY}""
+fi
+
 popd
+
