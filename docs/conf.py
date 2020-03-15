036766e846a72e9d3c82bc8ab416133916b1416f
Franco Masotti <franco.masotti@live.com>
2020-03-15 16:48:39 +0100

    Preparing for new release.

--- docs/conf.py
+++ docs/conf.py
@@ -22,9 +22,9 @@ copyright = '2019-2020, Franco Masotti'
 author = 'Franco Masotti'
 
 # The short X.Y version.
-version = '2.0.1'
+version = '2.1.0'
 # The full version, including alpha/beta/rc tags
-release = '2.0.1'
+release = '2.1.0'
 
 
 # -- General configuration ---------------------------------------------------
