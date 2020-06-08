54b5dbc4e1926ef5ac1a5126ae7addda94c76613
Franco Masotti <franco.masotti@live.com>
2020-06-08 18:01:08 +0200

    Updated dependencies.

--- docs/conf.py
+++ docs/conf.py
@@ -22,9 +22,9 @@ copyright = '2019-2020, Franco Masotti'
 author = 'Franco Masotti'
 
 # The short X.Y version.
-version = '2.4.0'
+version = '3.0.0'
 # The full version, including alpha/beta/rc tags
-release = '2.4.0'
+release = '3.0.0'
 
 
 # -- General configuration ---------------------------------------------------
