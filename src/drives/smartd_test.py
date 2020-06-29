ee031e0b193bf2d064d4c4ac0d829558746e7d37
Franco Masotti <franco.masotti@live.com>
2020-06-29 19:28:09 +0200

    Fixed a script.

--- src/drives/smartd_test.py
+++ src/drives/smartd_test.py
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import fpyutils
 import re
 import configparser
 import sys
@@ -25,53 +26,78 @@ import pathlib
 import subprocess
 import json
 import shlex
-import requests
 
 # Constants.
 # These need to be checked.
-STATUS_IDLE=0
-STATUS_BUSY=249
+STATUS_IDLE = 0
+STATUS_BUSY = 249
 
 # You must stop programs like hd-idle before performing these tests.
+
 
 class UserNotRoot(Exception):
     """The user running the script is not root."""
 
+
 class NoDisksConfigured(Exception):
     """No disks configured."""
 
-def get_disks():
-    disks=list()
+
+def get_disks() -> list:
+    r"""Scan all the disks."""
+    disks = list()
     for d in pathlib.Path('/dev/disk/by-id').iterdir():
         # Ignore disks ending with part-${integer} to avoid duplicates (names
         # corresponding to partitions of the same disk).
         disk = str(d)
         if re.match('.+-part[0-9]+$', disk) is None:
             try:
-               ddict = json.loads(subprocess.run(shlex.split('smartctl --all --json ' + shlex.quote(disk)), capture_output=True, check=True, timeout=30).stdout)
-               try:
-                   # Check for smart test support.
-                   if ddict['ata_smart_data']['capabilities']['self_tests_supported']:
-                       disks.append(disk)
-               except KeyError:
-                   pass
+                ddict = json.loads(
+                    subprocess.run(shlex.split('smartctl --all --json ' +
+                                               shlex.quote(disk)),
+                                   capture_output=True,
+                                   check=True,
+                                   timeout=30).stdout)
+                try:
+                    # Check for smart test support.
+                    if ddict['ata_smart_data']['capabilities'][
+                            'self_tests_supported']:
+                        disks.append(disk)
+                except KeyError:
+                    pass
             except subprocess.TimeoutExpired:
-                print ('timeout for ' + disk)
+                print('timeout for ' + disk)
             except subprocess.CalledProcessError:
-                print ('device ' + disk + ' does not support S.M.A.R.T. commands, skipping...')
+                print('device ' + disk +
+                      ' does not support S.M.A.R.T. commands, skipping...')
 
     return disks
 
-def disk_ready(disk: str):
+
+def disk_ready(disk: str) -> bool:
+    r"""Check if the disk is ready."""
     # Raises a KeyError if disk has not S.M.A.R.T. status capabilities.
-    ddict = json.loads(subprocess.run(shlex.split('smartctl --all --json ' + shlex.quote(disk)), capture_output=True, check=True, timeout=30).stdout)
+    ddict = json.loads(
+        subprocess.run(shlex.split('smartctl --all --json ' +
+                                   shlex.quote(disk)),
+                       capture_output=True,
+                       check=True,
+                       timeout=30).stdout)
     if ddict['ata_smart_data']['self_test']['status']['value'] != STATUS_BUSY:
         return True
     else:
         return False
 
-def run_test(disk: str, test_length: str = 'long'):
-    return subprocess.run(shlex.split('smartctl --test=' + shlex.quote(test_length) +  ' ' + shlex.quote(disk)), capture_output=True, check=True, timeout=30).stdout
+
+def run_test(disk: str, test_length: str = 'long') -> str:
+    r"""Run the smartd test."""
+    return subprocess.run(
+        shlex.split('smartctl --test=' + shlex.quote(test_length) + ' ' +
+                    shlex.quote(disk)),
+        capture_output=True,
+        check=True,
+        timeout=30).stdout
+
 
 if __name__ == '__main__':
     if os.getuid() != 0:
@@ -93,20 +119,15 @@ if __name__ == '__main__':
         if config.getboolean(s, 'enable') and dev in disks_available:
             if disks_to_check == 'all' or disks_to_check == s:
                 if disk_ready(dev):
-                    print ('attempting ' + s + ' ...')
+                    print('attempting ' + s + ' ...')
                     message = run_test(dev, config[s]['test']).decode('utf-8')
-                    print (message)
+                    print(message)
                     if config.getboolean(s, 'log to gotify'):
-                        # A very simple string concatenation to compute the URL. It is up to the user to
-                        # configure the variables correctly.
-                        full_url = config[s]['gotify url'] + 'message?token=' + config[s]['gotify token']
-                        payload = dict()
-                        payload['title'] = config[s]['gotify title']
-                        payload['message'] = config[s]['gotify message'] + '\n\n' + message
-                        payload['priority'] = int(config[s]['gotify priority'])
-                        resp = requests.post(full_url, json=payload)
+                        fpyutils.notify.send_gotify_message(
+                            config[s]['gotify url'], config[s]['gotify token'],
+                            message, config[s]['gotify title'],
+                            int(config[s]['gotify priority']))
                 else:
                     # Drop test requests if a disk is running a test in a particular moment.
                     # This avoid putting the disks under too much stress.
-                    print ('disk ' + s + ' not ready, checking the next...')
-
+                    print('disk ' + s + ' not ready, checking the next...')
